bae15f169f49b685cf875b749bcae6f4fd026950
Yuwenbiao <ywb1996@189.cn>
2019-05-29 09:06:43 +0800

    将序列分解为单独的变量

---
+++ 将序列分解为单独的变量.py
@@ -0,0 +1,12 @@
+p=(4,5)
+x,y=p
+
+data=['ACME',50,91.1,(2012,12,21)]
+name,shares,price,data=data
+name,shares,price,(year,mon,day)=data
+
+s='Hello'
+a,b,c,d,e=s
+
+data=['ACME',50,91.1,(2012,12,21)]
+-,shares,price,_=data
